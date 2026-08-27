d81b9547190926df3a6127abd26decc0dddf57b4
tetedoceara <you@example.com>
2026-08-27 14:36:45 -0300

    .,

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 const games = [
   {
-    category: "PORCENTAGEM",
-    title: "Percent Soul",
+    category: "A CATEGORIA DO JOGO",
+    title: "NOME DO JOGO",
     desc: "SOBRE O JOGO.",
-    image:"PercentSoul.jpg",
-    link: "https://percentsoul.vercel.app/",
+    image: "https://via.placeholder.com/400x280/1D1F5A/FFFFFF?text=Math+Matrix",
+    info: "INFORMAÇÕES DO JOGO",
+    link: "#",
   },
   {
     category: "A CATEGORIA DO JOGO",
     title: "NOME DO JOGO",
     desc: "SOBRE O JOGO.",
     image: "https://via.placeholder.com/400x280/1D1F5A/FFFFFF?text=Math+Matrix",
+    info: "INFORMAÇÕES DO JOGO",
     link: "#",
   },
   {
-    category: "A CATEGORIA DO JOGO",
-    title: "NOME DO JOGO",
-    desc: "SOBRE O JOGO.",
-    image:
-      "https://via.placeholder.com/400x280/0d0e29/FFFFFF?text=Retro+Runner",
-    link: "#",
+    category: "PORCENTAGEM",
+    title: "Percent Soul",
+    desc: "Uma aula de revisão, um dia antes da prova. O protagonista dorme e vai parar em um pesadelo com inimigos da matemática e batalhas no estilo bullet hell, inspiradas em Undertale.",
+    image:"PercentSoul.jpg",
+    info: "DISNPONÍVEL PARA CELULAR E COMPUTADOR",
+    link: "https://percentsoul.vercel.app/",
   },
   {
-    category: "A CATEGORIA DO JOGO",
-    title: "NOME DO JOGO",
-    desc: "SOBRE O JOGO.",
-    image:
-      "https://via.placeholder.com/400x280/0d0e29/FFFFFF?text=Retro+Runner",
-    link: "#",
+    category: "PLANO CARTESIANO",
+    title: "Mira no Queijo",
+    desc: "Em meio à disputa pela sobrevivência no esgoto, os calangos surgem e levam o queijo dos ratos, dando início a uma perseguição. Para reconquistar o que foi perdido, os ratos precisarão contar com seus conhecimentos de plano cartesiano, analisando posições e quadrantes para localizar seus adversários e acertar os alvos.",
+    image:"rat.jpg",
+    info: "DISPONÍVEL APENAS NO COMPUTADOR",
+    link: "https://ratgame-azure.vercel.app",
   },
   {
-    category: "A CATEGORIA DO JOGO",
-    title: "NOME DO JOGO",
-    desc: "SOBRE O JOGO.",
-    image:
-      "https://via.placeholder.com/400x280/0d0e29/FFFFFF?text=Retro+Runner",
-    link: "#",
+    category: "RAZÃO E PROPORÇÃO",
+    title: "Reino das Proporções",
+    desc: "O equilíbrio de um mundo foi ameaçado pelo Rei das Trevas, e você assume o papel do herói encarregado de deter esse vilão. Ao longo da jornada, é preciso enfrentar inimigos e resolver desafios de razão e proporção para se aproximar do confronto final no castelo, derrotar o vilão e restaurar a harmonia matemática do reino.",
+    image:"reino.jpg",
+    info: "DISPONÍVEL APENAS NO COMPUTADOR",
+    link: "https://reinodasproporcoes.vercel.app/",
   },
 ];
 
@@ -45,6 +47,7 @@ const gameCategory = document.getElementById("gameCategory");
 const gameTitle = document.getElementById("gameTitle");
 const gameDesc = document.getElementById("gameDesc");
 const gameImage = document.getElementById("gameImage");
+const gameInfo = document.getElementById("gameInfo");
 const gameLink = document.getElementById("gameLink");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
@@ -71,6 +74,7 @@ function showGame(index) {
   gameTitle.innerText = game.title;
   gameDesc.innerText = game.desc;
   gameImage.src = game.image;
+  gameInfo.innerText = game.info;
   gameLink.href = game.link;
 
   createDots();
